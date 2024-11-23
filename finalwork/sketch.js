adccd91318773be08e2a8f943ce1736f998a6a9d
KOSUKE217 <2300841h@cloud.kobe-u.jp>
2024-11-23 12:45:43 +0900

    Finalwork

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,13 +1,65 @@
 // 最終課題を制作しよう
 
+let bubbles=[];
 function setup(){
   createCanvas(windowWidth, windowHeight);
+  fill(120,65,56);
+  background(230, 230, 255);
 }
 
 function draw(){
-  background(160, 192, 255);
-}
+  background(149, 192, 236);
+  drawGround();
+  drawFrog();
+  drawShabonstick();
+  for (let i = bubbles.length - 1; i >= 0; i--) {
+    let bubble = bubbles[i];
+    bubble.x += bubble.vx; // x方向に移動
+    bubble.y += bubble.vy; // y方向に移動
 
+    push();
+    fill(255, 255, 255, 150); // 半透明の白色
+    noStroke();
+    ellipse(bubble.x, bubble.y, bubble.size); // シャボン玉を描画
+    pop();
+    // 画面外に出たら配列から削除
+    if (bubble.x > width || bubble.y < 0) {
+      bubbles.splice(i, 1);
+    }
+  }
+  }
+  function keyPressed() {
+    // 新しいシャボン玉を追加
+    let newBubble = {
+      x: width / 2 + 110,        // スティックの位置
+      y: height - 140,
+      vx: random(2, 4),          // x方向の速度（右上）
+      vy: random(-4, -2),        // y方向の速度（上）
+      size: random(20, 50)       // シャボン玉の大きさ
+    };
+    bubbles.push(newBubble); // 配列に追加
+  }
+function drawFrog(){
+  fill(30,180,60);
+  ellipse(width/2,height-75,100,50);
+  ellipse(width/2-30,height-100,30,30);
+  ellipse(width/2+30,height-100,30,30);
+  fill(0);
+  ellipse(width/2-30,height-100,5,5);
+  ellipse(width/2+30,height-100,5,5);
+  fill(255,0,0);
+  ellipse(width/2,height-75,10,10);
+  
+}
+function drawShabonstick(){
+  fill(230,230,255)
+  ellipse(width/2+110,height-140,50,50);
+  rect(width/2+105,height-115,10,65);
+}
+function drawGround(){
+  fill(160,110,45)
+  rect(0,height-50,width,50)
+}
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
 }
--- section3-3/sketch.js
+++ section3-3/sketch.js
@@ -1,5 +1,7 @@
 // テキスト「キーボード操作に反応する」
 let x, y;
+//50<x<width-50;
+//50<y<height*0.8-50;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
@@ -9,13 +11,24 @@ function setup(){
 
 function draw(){
   background(160, 192, 255);
+  fill(120,65,56)
+  rect(0,height*0.8,width,height*0.2)
+  fill(100,100,0)
   ellipse(x, y, 50);
+  if(x>width){x=0;}
+  else if (x<0){x=width;}
+
+
+
   if(keyIsDown(LEFT_ARROW)){ x -= 5; }
   if(keyIsDown(RIGHT_ARROW)){ x += 5; }
   if(keyIsDown(UP_ARROW)){ y -= 5; }
-  if(keyIsDown(DOWN_ARROW)){ y += 5; }
-  if(keyIsDown("A".charCodeAt(0))){ x+= 10; }
-  if(keyIsDown(" ".charCodeAt(0))){ x-= 10; }
+  if(keyIsDown(DOWN_ARROW)&y<height*0.8-25){ y += 5; }
+  //if(keyIsDown("R".charCodeAt(0))){ x+= 10; }
+  //if(keyIsDown("L".charCodeAt(0))){ x-= 10; }
+  
+  if(keyIsDown(RIGHT_ARROW) && keyIsDown("R".charCodeAt(0))){x+=10;}
+  if(keyIsDown(LEFT_ARROW) && keyIsDown("L".charCodeAt(0))){x-=10;}
 }
 
 // イベントハンドラを使用するパターン
